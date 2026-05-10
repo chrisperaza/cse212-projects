f578f1e0fb07d467b62fcd5586ce658e7f923c5d
Christian Peraza <christianperaza2000@gmail.com>
2026-05-09 21:02:28 -0300

    W01 code activity completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,10 +13,15 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        // 1 - Create List with length
-        // 2 - Create a loop to iterate number by number
-        // 3 - Add a condition that multiply the "number" by each number
-        // 4 - Add results that condition into the list
+        // PLAN:
+        // 1 - create "results" array to store the result elements with a fixed size that is determined by length
+        // 2 - create variable "listIndex" to add the multiples to the "results" array
+        // 3 - make a loop that starts at 1 to skip the multiple 0 and repeats the number of times determined by length
+        // 4 - multiply number by each integer number to find the multiples and store it in a variable
+        // 5 - add a condition that evaluates whether the remainder of multiple between number is zero to verify that it is indeed a multiple
+        // 6 - if the condition from the previous step is met, add the multiple to the results array
+        // 7 - plus 1 to the listIndex variable so that the next time you loop through the loop, the multiple is added to the next available array space
+        // 8 - return the results array
 
         var results = new double[length];
         int listIndex = 0;
@@ -25,7 +30,10 @@ public static class Arrays
         {
             double multiple = number * i;
 
-            results[listIndex] = multiple;
+            if (multiple % number == 0)
+            {
+                results[listIndex] = multiple;
+            }
 
             listIndex++;
         }
@@ -41,11 +49,26 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
-    public static void RotateListRight(List<int> data, int amount)
+    public static List<int> RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // PLAN:
+        // 1 - make a loop that starts at zero and the repetition of the loop is determined by "amount" to rotate the list as many times as determined by "amount"
+        // 2 - get the last element of the list "data" and store it in a variable
+        // 3 - use the Insert() method to place the value of the last element in the first position of the list and move the list to the right
+        // 4 - use the RemoveAt() method to remove the last element from the list
+        // 5 - return the modified data list
+
+        for (int i = 0; i < amount; i++) {
+			int lastNumber = data.Last();
+			data.Insert(0, lastNumber);
+			data.RemoveAt(data.Count - 1);
+		}
+		
+		return data;
     }
 }
